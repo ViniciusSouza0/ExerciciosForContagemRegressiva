9c9913293e8d5a5f4cb35d7224bcbcc39cf2513f
aluno <vinicius.s.silveira.2015@gmail.com>
2024-03-14 19:04:16 -0300

    Novo9

---
+++ src/main/java/com/mycompany/exerciciosforcontagemregressiva/ExerciciosForcontagemregressiva.java
@@ -0,0 +1,29 @@
+package com.mycompany.exerciciosforcontagemregressiva;
+import java.util.Scanner;
+public class ExerciciosForcontagemregressiva {
+public static void main(String[] args) {
+    
+    
+    
+//Contagem regressiva   
+Scanner teclado = new Scanner(System.in);
+int x = teclado.nextInt();
+for (int i=x; i>=1; i--){
+System.out.println(i);}
+}
+}
+
+//Contagem progressiva
+
+//Scanner teclado = new Scanner(System.in);
+//int x = teclado.nextInt();
+//for (int i=x; i<=100; i++){
+//System.out.println(i);}
+//}
+//}
+    
+    
+    
+    
+    
+
